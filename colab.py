88375c917c7f67503080e6423e028118e1656f89
Reeshabh Chhatait <chhataitreeshabh14111997@gmail.com>
2021-05-12 19:14:25 +0530

    Update colab.py

--- colab.py
+++ colab.py
@@ -1,6 +1,6 @@
 success = 0   #variable for storing number of event occurence
 for i in range(1000000): #repetitions
-    runs = uniform(6,5) #Less than 6 deliveries = 5
+    runs = uniform(7,5) #Less than 6 deliveries = 5
     for run in runs:
         if run == 6:
            success = success + 1
