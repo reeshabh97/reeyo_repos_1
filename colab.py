e9cf07b2c23e6b26c0a3a52e9b1d72e29e22b3c1
Reeshabh Chhatait <chhataitreeshabh14111997@gmail.com>
2021-05-12 18:00:02 +0530

    Create colab.py

---
+++ colab.py
@@ -0,0 +1,8 @@
+success = 0   #variable for storing number of event occurence
+for i in range(1000000): #repetitions
+    runs = uniform(6,5) #Less than 6 deliveries = 5
+    for run in runs:
+        if run == 6:
+           success = success + 1
+           break #We need to succeed at most once in a given list of runs[].
+print(success/1000000) #probability estimate by Monte Carlo
